3c8b2bda6a484efa9f2f9172d81088538b8aaa2a
andrei-mortl <mor.tl08201988@gmail.com>
2024-12-11 18:41:19 +0300

    Add files via upload

---
+++ module_8/module_8_1.py
@@ -0,0 +1,11 @@
+def add_everything_up(a,b):
+    try:
+        a + b
+    except TypeError as exp:
+        return (f'{a}{b}')
+    else:
+        return f"{a+b:.6g}"
+print(add_everything_up(123.456, 'строка'))
+print(add_everything_up('яблоко', 4215))
+print(add_everything_up(123.456, 7))
+
